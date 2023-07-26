4b63bea0ae5a41c5a0646d3fdf7c144ad1da7a3c
Joao Pedro <jp.reisconde@gmail.com>
2023-07-26 17:19:43 -0300

    New: adição de comentarios.

--- script.js
+++ script.js
@@ -4,21 +4,26 @@ const segundos = document.getElementById('segundos');
 const body = document.querySelector('body');
 
 const relogio = setInterval(function time() {
+  /*Variavel que armazena as horas do sistema*/
   let dateToday = new Date();
+  /*Variaveis que recebem as informações do horario*/
   let hora = dateToday.getHours();
   let minuto = dateToday.getMinutes();
   let segundo = dateToday.getSeconds();
 
+  /*Condições para adicionar 0 quando o valor for de 0 a 9*/
   if(hora < 10) hora = "0" + hora;
 
   if(minuto < 10) minuto = "0" + minuto;
 
   if(segundo < 10) segundo = "0" + segundo;
 
+  /*Indicando os valores para as inputs correspondente*/
   horas.textContent = hora;
   minutos.textContent = minuto;
   segundos.textContent = segundo;
 
+  /*Condições para adicionar a propriedade dark no body*/
   if(hora > 17 || hora < 6){
     body.classList.add('dark');
   } else{
